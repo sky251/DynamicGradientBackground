5e165a9c76ccd481f6a660bc693eb1c4ed203afe
Akash Patel <patelakash56964@gmail.com>
2025-04-10 16:04:24 -0400

    second

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let btn1 = document.getElementById("myButton")
 let btn2 = document.getElementById("myButton2")
+let leftBtn = document.getElementsByClassName("leftButton")[0]
+let rightBtn = document.getElementsByClassName("rightButton")[0]
 let rgb = "#051937"
 let rgb2 = "#008793"
+
 
 const getHexValue = () => {
     let color = "#"
@@ -16,16 +19,19 @@ const getHexValue = () => {
 const handleBtn1 = () => {
     rgb = getHexValue()
     btn1.innerText = rgb
-    console.log(rgb);
+    btn1.style.backgroundColor = rgb
     document.body.style.backgroundImage = `linear-gradient(to right, ${rgb}, ${rgb2})`
+    rightBtn.innerText = rgb
+    
 }
 const handleBtn2 = () => {
     rgb2 = getHexValue()
+    btn2.innerText = rgb2
+    btn2.style.backgroundColor = rgb2
     document.body.style.backgroundImage = `linear-gradient(to right, ${rgb}, ${rgb2})`
+    leftBtn.innerText = rgb2
 }
 
-console.log(btn1)
+
 btn1.addEventListener("click", handleBtn1)
 btn2.addEventListener("click", handleBtn2)
-
-
